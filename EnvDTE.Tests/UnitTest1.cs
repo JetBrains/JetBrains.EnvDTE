e22687360d07aa2c1731571ca4be5fdb913194be
Kirill Glazyrin <kirillgla@gmail.com>
2019-09-19 12:20:34 +0300

    - added a first draft of tests

---
+++ EnvDTE.Tests/UnitTest1.cs
@@ -0,0 +1,38 @@
+using JetBrains.Annotations;
+using JetBrains.EnvDTE.Host;
+using JetBrains.Lifetimes;
+using NUnit.Framework;
+
+namespace JetBrains.EnvDTE.Tests
+{
+    public sealed class Tests
+    {
+        [NotNull]
+        private LifetimeDefinition Definition { get; set; }
+
+        [NotNull]
+        private ConnectionManager HostConnectionManager { get; set; }
+
+        [NotNull]
+        private Client.ConnectionManager ClientConnectionManager { get; set; }
+
+        [SetUp]
+        public void Setup()
+        {
+            Definition = new LifetimeDefinition();
+            // TODO: wtf it throws compilation error?
+            // Lifetime library should've been referenced through EnvDTE.Client project
+            HostConnectionManager = new ConnectionManager(Definition.Lifetime, null);
+            ClientConnectionManager = new Client.ConnectionManager(Definition.Lifetime, HostConnectionManager.Port);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void TearDown() => Definition.Terminate();
+    }
+}
