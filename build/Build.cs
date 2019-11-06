c3832ef50eca5df68674940ec447932c0a3d07b8
Kirill Glazyrin <Kirill.Glazyrin@gmail.com>
2019-11-06 20:08:21 +0300

    - added nuke build

---
+++ build/Build.cs
@@ -0,0 +1,70 @@
+using Nuke.Common;
+using Nuke.Common.Execution;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.NuGet;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.Tools.NuGet.NuGetTasks;
+
+[CheckBuildProjectConfigurations]
+[UnsetVisualStudioEnvironmentVariables]
+sealed class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+    public static int Main() => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution] readonly Solution Solution;
+    private AbsolutePath TestsDirectory => RootDirectory / "tests";
+    private AbsolutePath ArtifactsDirectory => RootDirectory / "artifacts";
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(ArtifactsDirectory);
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(_ => _
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(_ => _
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .EnableNoRestore());
+        });
+
+    public Target Pack => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            PackProject("JetBrains.EnvDTE.Client.nuspec");
+            PackProject("JetBrains.EnvDTE.Host.nuspec");
+            PackProject("JetBrains.EnvDTE.nuspec");
+        });
+
+    public void PackProject(string projectName) =>
+        NuGetPack(s => s
+            .SetTargetPath(RootDirectory / "NuGet" / (projectName))
+            .SetBasePath(RootDirectory)
+            .SetOutputDirectory(ArtifactsDirectory)
+            .EnableNoPackageAnalysis());
+}
