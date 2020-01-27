43e56138fd40d36a6946bc599fb5da91c9ab1486
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-01-27 19:47:22 +0300

    Delegate to Nuke build

--- CI/build.ps1
+++ CI/build.ps1
@@ -3,13 +3,13 @@ Try {
     .\gradlew
     $code = $LastExitCode
     Write-Host "Gradle finished: $code"
-    if ($code -ne 0) { throw "Error: Unable to generate protocole model: gradlew exit code $code" }
+    if ($code -ne 0) { throw "Error: Unable to generate protocol model: gradlew exit code $code" }
 }
 Finally {
     Pop-Location
 }
 
-msbuild
+./build.ps1
 $code = $LastExitCode
-Write-Host "msbuild finished: $code"
-if ($code -ne 0) { throw "Error: unable to build project: msbuild exit code $code" }
+Write-Host "Nuke build finished: $code"
+if ($code -ne 0) { throw "Error: unable to build project: Nuke exit code $code" }
