bbf7b5a1c9ff6734518a2d242e213c052d4d60ff
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-01-28 10:08:51 +0300

    Pack nuget files in buildscript

--- CI/build.ps1
+++ CI/build.ps1
@@ -11,3 +11,15 @@ Finally {
 
 dotnet restore
 dotnet msbuild
+$packages = @(
+  "JetBrains.EnvDTE.Client.nuspec",
+  "JetBrains.EnvDTE.Host.nuspec",
+  "JetBrains.EnvDTE.nuspec",
+  "JetBrains.EnvDTE80.nuspec",
+  "JetBrains.EnvDTE90.nuspec",
+  "JetBrains.EnvDTE90a.nuspec",
+  "JetBrains.EnvDTE100.nuspec")
+
+foreach ($package in $packages) {
+    nuget pack $package -BasePath .
+}
