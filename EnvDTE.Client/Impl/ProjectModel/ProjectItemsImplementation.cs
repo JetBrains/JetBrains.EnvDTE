d37be08ca4733bc349ac2cfcc78d301fc1e297fa
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-03-17 00:11:47 +0300

    - implemented ProjectItems indexer

--- EnvDTE.Client/Impl/ProjectModel/ProjectItemsImplementation.cs
+++ EnvDTE.Client/Impl/ProjectModel/ProjectItemsImplementation.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using EnvDTE;
+using JetBrains.Annotations;
 using JetBrains.Rider.Model;
 
 namespace JetBrains.EnvDTE.Client.Impl.ProjectModel
@@ -25,13 +26,21 @@ namespace JetBrains.EnvDTE.Client.Impl.ProjectModel
         public DTE DTE => Implementation;
         public object Parent { get; }
         public int Count => ProjectItemModels.Count;
-        public string Kind => throw new NotImplementedException();
-        public Project ContainingProject => throw new NotImplementedException();
-        public ProjectItem Item(object index) => throw new NotImplementedException();
+
+        [CanBeNull]
+        public ProjectItem Item(object index)
+        {
+            if (!(index is int number)) return null;
+            var itemModel = ProjectItemModels.ElementAtOrDefault(number);
+            if (itemModel == null) return null;
+            return new ProjectItemImplementation(Implementation, itemModel);
+        }
 
         IEnumerator ProjectItems.GetEnumerator() =>
             ProjectItemModels.Select(model => new ProjectItemImplementation(Implementation, model)).GetEnumerator();
 
+        public string Kind => throw new NotImplementedException();
+        public Project ContainingProject => throw new NotImplementedException();
         public ProjectItem AddFromFile(string FileName) => throw new NotImplementedException();
         public ProjectItem AddFromTemplate(string FileName, string Name) => throw new NotImplementedException();
         public ProjectItem AddFromDirectory(string Directory) => throw new NotImplementedException();
