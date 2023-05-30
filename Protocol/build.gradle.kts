130c3a839c4e6d47032bc728955ba3d4489a75a2
Ivan Migalev <ivan.migalev@jetbrains.com>
2023-05-30 16:18:23 +0200

    gradle-jvm-wrapper: update to 0.14.0

--- Protocol/build.gradle.kts
+++ Protocol/build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
 plugins {
   id("org.jetbrains.intellij") version "1.4.0"
   kotlin("jvm") version "1.4.10"
-  id("me.filippov.gradle.jvm.wrapper") version "0.9.3"
+  id("me.filippov.gradle.jvm.wrapper") version "0.14.0"
 }
 
 apply {
