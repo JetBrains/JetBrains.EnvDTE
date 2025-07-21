4db85f94a4b806cd726642104f85d9730cad54ae
Andrii Rublov <seclerp.dev@gmail.com>
2025-07-21 16:13:54 +0200

    EnvDTE: Replace .NET SDK installation logic with `mfilippov/command-line-wrappers/dotnet.cmd`, bump .NET SDK version to 9.0.303

--- build.ps1
+++ build.ps1
@@ -1,18 +1,6 @@
-$dotNetSdkUrl = "https://download.visualstudio.microsoft.com/download/pr/f090672d-ffb0-4126-8154-68649cc55ad4/a09964d24b1bf583ad2f283f84c0a89c/dotnet-sdk-3.1.109-win-x64.zip"
-
-$dotNetSdkPath = Join-Path $PSScriptRoot ".dotnet"
 $progressPreference = 'SilentlyContinue'
 
-if (!(Test-Path $dotNetSdkPath))
-{
-    $archivePath = Join-Path $Env:Temp "$(New-Guid).zip"
-    $tempFolderPath = Join-Path $Env:Temp $(New-Guid)
-    (New-Object System.Net.WebClient).DownloadFile($dotNetSdkUrl, $archivePath)
-    New-Item $tempFolderPath -ItemType Directory
-    Expand-Archive -Path $archivePath -DestinationPath $tempFolderPath
-    Move-Item $tempFolderPath $dotNetSdkPath
-    Remove-Item $archivePath
-}
+./dotnet.cmd --version
 
 Push-Location -Path Protocol
 Try {
@@ -29,6 +17,14 @@ if ($Env:PackageVersion -ne "") {
     $packageVersion = $Env:PackageVersion
 }
 
-&"$dotNetSdkPath\dotnet.exe" pack /p:Configuration=Release "$PSScriptRoot\EnvDTE100.Interfaces\EnvDTE100.Interfaces.csproj"
+Write-Host "Building EnvDTE.Processor"
+
+./dotnet.cmd build EnvDTE.Processor
 $code = $LastExitCode
-if ($code -ne 0) { throw "Could not build solution" }
+if ($code -ne 0) { throw "Could not build EnvDTE.Processor" }
+
+Write-Host "Packing interfaces"
+
+./dotnet.cmd pack /p:Configuration=Release "$PSScriptRoot\EnvDTE100.Interfaces\EnvDTE100.Interfaces.csproj"
+$code = $LastExitCode
+if ($code -ne 0) { throw "Could not pack interfaces" }
