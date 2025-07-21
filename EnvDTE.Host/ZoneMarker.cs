8d80c6b48410db073c7cce55ec9b6b981301c2eb
Andrii Rublov <seclerp.dev@gmail.com>
2025-07-21 16:31:11 +0200

    EnvDTE: Introduce zoning to shut-up some inspections

---
+++ EnvDTE.Host/ZoneMarker.cs
@@ -0,0 +1,7 @@
+﻿using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.RdBackend.Common.Env;
+
+namespace JetBrains.EnvDTE.Host;
+
+[ZoneMarker]
+public class ZoneMarker :  IRequire<IReSharperHostCoreFeatureZone>;
