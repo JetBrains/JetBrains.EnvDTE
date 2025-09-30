e38ddb357ac59f40c666dd38459e6d5aa31d856d
Veljko Dragic <veljkodragic12@gmail.com>
2025-09-30 03:25:31 +0200

    Refactor gradle setup and update dependencies

--- Protocol/build.gradle.kts
+++ Protocol/build.gradle.kts
@@ -1,139 +1,136 @@
-import com.jetbrains.rd.generator.gradle.RdGenExtension
-import org.jetbrains.kotlin.daemon.common.toHexString
+import com.jetbrains.plugin.structure.base.utils.isFile
+import org.jetbrains.intellij.platform.gradle.Constants
+import kotlin.io.path.absolute
+import kotlin.io.path.isDirectory
 
-// Some build script code borrowed from F# and T4 plugins
-
-buildscript {
-  repositories {
-    maven { setUrl("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies") }
-    maven { setUrl("https://cache-redirector.jetbrains.com/maven-central") }
-  }
-  dependencies {
-    // https://search.maven.org/artifact/com.jetbrains.rd/rd-gen
-    classpath("com.jetbrains.rd:rd-gen:2025.2.2")
-  }
-}
 
 plugins {
-  id("org.jetbrains.intellij") version "1.13.3"
-  id("org.jetbrains.kotlin.jvm") version "1.8.10"
-  id("me.filippov.gradle.jvm.wrapper") version "0.15.0"
-}
+    alias(libs.plugins.gradleIntelliJPlatform)
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.changelog)
 
-apply {
-  plugin("com.jetbrains.rdgen")
-}
-
-repositories {
-  mavenCentral()
+    alias(libs.plugins.gradleJvmWrapper)
+    id("com.jetbrains.rdgen") version libs.versions.rdGen
 }
 
 dependencies {
-  implementation(kotlin("stdlib"))
-  implementation(kotlin("reflect"))
+    implementation(libs.rdGen)
+    intellijPlatform {
+        rider(libs.versions.riderSdk) {
+            useInstaller = false
+        }
+    }
+    implementation(libs.kotlinStdLib)
 }
 
-java {
-  sourceCompatibility = JavaVersion.VERSION_1_8
-  targetCompatibility = JavaVersion.VERSION_1_8
+repositories {
+    mavenCentral()
 }
 
-val baseVersion = "2025.1.4"
-version = baseVersion
+version = libs.versions.riderSdk
 
-intellij {
-  type.set("RD")
-  version.set("$baseVersion")
-  instrumentCode.set(false)
-  downloadSources.set(false)
-  updateSinceUntilBuild.set(false)
-  // Workaround for https://youtrack.jetbrains.com/issue/IDEA-179607
-  plugins.set(listOf("rider-plugins-appender"))
+intellijPlatform {
+    instrumentCode = false
 }
 
 val repoRoot = projectDir.parentFile!!
 
-configure<RdGenExtension> {
-  val hostOutput = File(repoRoot, "EnvDTE.Host/Protocol")
-  val clientOutput = File(repoRoot, "EnvDTE.Client/Protocol")
+rdgen {
+    val hostOutput = File(repoRoot, "EnvDTE.Host/Protocol")
+    val clientOutput = File(repoRoot, "EnvDTE.Client/Protocol")
 
-  verbose = true
-  hashFolder = "build/rdgen"
-  classpath({
-    val sdkPath = tasks.setupDependencies.get().idea.get().classes
-    val rdLibDirectory = File(sdkPath, "lib/rd").canonicalFile
-    "$rdLibDirectory/rider-model.jar"
-  })
-  sources(File(repoRoot, "Protocol/src/main/kotlin/model"))
-  packages = "model"
+    verbose = true
+    hashFolder = "build/rdgen"
+    sources(File(projectDir, "src/main/kotlin/model"))
+    packages = "model"
 
-  generator {
-    language = "csharp"
-    transform = "asis"
-    root = "model.DteRoot"
-    namespace = "JetBrains.Rider.Model"
-    directory = "$clientOutput"
-  }
+    generator {
+        language = "csharp"
+        transform = "asis"
+        root = "model.DteRoot"
+        namespace = "JetBrains.Rider.Model"
+        directory = "$clientOutput"
+    }
 
-  generator {
-    language = "csharp"
-    transform = "reversed"
-    root = "model.DteRoot"
-    namespace = "JetBrains.Rider.Model"
-    directory = "$hostOutput"
-  }
+    generator {
+        language = "csharp"
+        transform = "reversed"
+        root = "model.DteRoot"
+        namespace = "JetBrains.Rider.Model"
+        directory = "$hostOutput"
+    }
 }
 
 val dotNetSdkPath by lazy {
-  val sdkPath = tasks.setupDependencies.get().idea.get().classes.resolve("lib").resolve("DotNetSdkForRdPlugins")
-  if (sdkPath.isDirectory.not()) error("$sdkPath does not exist or not a directory")
+    val sdkPath = intellijPlatform.platformPath.resolve("lib/DotNetSdkForRdPlugins").absolute()
+    if (!sdkPath.isDirectory()) error("$sdkPath does not exist or not a directory")
 
-  println("SDK path: $sdkPath")
-  return@lazy sdkPath
-}
-
-val nugetConfigPath = File(repoRoot, "NuGet.Config")
-val dotNetSdkPathPropsPath = File("build", "DotNetSdkPath.generated.props")
-
-fun File.writeTextIfChanged(content: String) {
-  val bytes = content.toByteArray()
-  if (exists() && readBytes().toHexString() == bytes.toHexString()) return
-  writeBytes(bytes)
+    println("SDK path: $sdkPath")
+    return@lazy sdkPath
 }
 
 tasks {
-  wrapper {
-    gradleVersion = "8.1.1"
-    distributionType = Wrapper.DistributionType.ALL
-  }
-
-  create("writeDotNetSdkPathProps") {
-    doLast {
-      dotNetSdkPathPropsPath.writeTextIfChanged("""<Project>
+    val generateDotNetSdkProperties by registering {
+        val dotNetSdkPathPropsPath = File("build", "DotNetSdkPath.generated.props")
+        doLast {
+            dotNetSdkPathPropsPath.writeTextIfChanged(
+                """<Project>
   <PropertyGroup>
     <DotNetSdkPath>$dotNetSdkPath</DotNetSdkPath>
   </PropertyGroup>
 </Project>
 """
-      )
+            )
+        }
     }
-  }
-  create("writeNuGetConfig") {
-    doLast {
-      nugetConfigPath.writeTextIfChanged(
-        """<?xml version="1.0" encoding="utf-8"?>
+    val generateNuGetConfig by registering {
+        val nuGetConfigFile = File(repoRoot, "NuGet.Config")
+        doLast {
+            nuGetConfigFile.writeTextIfChanged(
+                """<?xml version="1.0" encoding="utf-8"?>
 <configuration>
   <packageSources>
     <add key="resharper-sdk" value="$dotNetSdkPath" />
   </packageSources>
 </configuration>
 """
-      )
+            )
+        }
     }
-  }
-  create("prepare") {
-    dependsOn("writeDotNetSdkPathProps", "writeNuGetConfig", "rdgen")
-  }
+
+    val rdGen = ":protocol:rdgen"
+
+    register("prepare") {
+        dependsOn(rdGen, generateDotNetSdkProperties, generateNuGetConfig)
+    }
 }
 
 defaultTasks("prepare")
+
+
+val riderModel: Configuration by configurations.creating {
+    isCanBeConsumed = true
+    isCanBeResolved = false
+}
+
+artifacts {
+    add(riderModel.name, provider {
+        intellijPlatform.platformPath.resolve("lib/rd/rider-model.jar").also {
+            check(it.isFile) {
+                "rider-model.jar is not found at $riderModel"
+            }
+        }
+    }) {
+        builtBy(Constants.Tasks.INITIALIZE_INTELLIJ_PLATFORM_PLUGIN)
+    }
+}
+
+fun File.writeTextIfChanged(content: String) {
+    val bytes = content.toByteArray()
+
+    if (!exists() || !readBytes().contentEquals(bytes)) {
+        println("Writing $path")
+        parentFile.mkdirs()
+        writeBytes(bytes)
+    }
+}
--- Protocol/settings.gradle.kts
+++ Protocol/settings.gradle.kts
@@ -1,1 +1,15 @@
 rootProject.name = "Protocol"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.jetbrains.rdgen") {
+                useModule("com.jetbrains.rd:rd-gen:${requested.version}")
+            }
+        }
+    }
+}
