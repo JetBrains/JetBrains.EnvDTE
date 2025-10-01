9c79571782f5c1d105c743f67bddbda77d6d9d02
Veljko Dragic <veljkodragic12@gmail.com>
2025-10-01 07:00:23 +0200

    Fix the problem where some MSBuild properties were not able to be read

---
+++ EnvDTE.Host/Callback/Impl/Properties/ProjectPropertyRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using JetBrains.Application;
+using JetBrains.Application.Parts;
+using JetBrains.ProjectModel.MSBuild;
+using JetBrains.ProjectModel.Properties;
+
+namespace JetBrains.EnvDTE.Host.Callback.Impl.Properties;
+
+// For some reason, these two are not readable through `IMsBuildPropertiesEditor`
+// I suspect that it is because they are computed based on other properties, and are not actual MSBuild properties
+// TODO: Investigate
+[ShellComponent(Instantiation.DemandAnyThreadSafe)]
+public class EnvDteProjectPropertyRequest : IProjectPropertiesRequest
+{
+    public IEnumerable<string> RequestedProperties => [
+        "TargetFileName",
+        MSBuildProjectUtil.TargetFrameworkMonikerProperty,
+    ];
+}
