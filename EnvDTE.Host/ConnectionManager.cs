4184c7863af491e47380264f2d83f0cdb6995ed6
Veljko Dragic <veljkodragic12@gmail.com>
2025-12-16 14:16:14 +0100

    Add support for multiple concurrent client connections

--- EnvDTE.Host/ConnectionManager.cs
+++ EnvDTE.Host/ConnectionManager.cs
@@ -15,27 +15,35 @@ namespace JetBrains.EnvDTE.Host
     {
         private const string Host = "EnvDTE Communication Host";
         private const string Protocol = "EnvDTE Communication Protocol";
-        private IReadOnlyList<IEnvDteCallbackProvider> CallbackProviders { get; }
         public int Port { get; private set; }
 
         public ConnectionManager(Lifetime lifetime, [NotNull] ISolution solution) => SetupModel(lifetime, solution);
 
         private void SetupModel(Lifetime lifetime, ISolution solution)
         {
-            var scheduler = SingleThreadScheduler.RunOnSeparateThread(lifetime, Host);
-            var server = new SocketWire.Server(lifetime, scheduler);
-            Port = server.Port;
-            var serializers = new Serializers();
-            var identities = new Identities(IdKind.Server);
-            scheduler.Queue(() =>
+            SingleThreadScheduler.RunOnSeparateThread(lifetime, Host, scheduler =>
             {
-                var protocol = new Protocol(Protocol, serializers, identities, scheduler, server, lifetime);
-                var model = new DteProtocolModel(lifetime, protocol);
-                RegisterCallbacks(model, solution);
+                var serverFactory = new SocketWire.ServerFactory(lifetime, scheduler);
+                Port = serverFactory.LocalPort;
+
+                serverFactory.Connected.View(lifetime, (connectionLifetime, server) =>
+                {
+                    var serializers = new Serializers();
+
+                    var protocol = new Protocol(Protocol, serializers, new Identities(IdKind.Server),
+                        scheduler, server, connectionLifetime);
+
+                    scheduler.Queue(() =>
+                    {
+                        var model = new DteProtocolModel(connectionLifetime, protocol);
+                        RegisterCallbacks(model, solution);
+                    });
+                });
+
             });
         }
 
-        private void RegisterCallbacks([NotNull] DteProtocolModel model, [NotNull] ISolution solution)
+        private static void RegisterCallbacks([NotNull] DteProtocolModel model, [NotNull] ISolution solution)
         {
             foreach (var provider in solution.GetComponents2<IEnvDteCallbackProvider>())
             {
