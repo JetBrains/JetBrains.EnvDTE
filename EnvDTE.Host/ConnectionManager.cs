35e67cf153320899a314c0f3b38844a38a070a38
Veljko Dragic <veljko.dragic@jetbrains.com>
2026-01-14 23:52:45 +0100

    Create a custom protocol factory for the EnvDTE host
    - The existing Rd `ServerFactory` fails to handle our use case and leads to exceptions after one of the client connections gets terminated and an attempt is made to open new client connections

--- EnvDTE.Host/ConnectionManager.cs
+++ EnvDTE.Host/ConnectionManager.cs
@@ -1,4 +1,5 @@
-using System.Collections.Generic;
+using System;
+using System.Net;
 using JetBrains.Annotations;
 using JetBrains.Collections.Viewable;
 using JetBrains.EnvDTE.Host.Callback;
@@ -23,16 +24,11 @@ namespace JetBrains.EnvDTE.Host
         {
             SingleThreadScheduler.RunOnSeparateThread(lifetime, Host, scheduler =>
             {
-                var serverFactory = new SocketWire.ServerFactory(lifetime, scheduler);
-                Port = serverFactory.LocalPort;
+                var serverFactory = new ProtocolFactory(lifetime, scheduler, Host);
+                Port = serverFactory.localPort;
 
-                serverFactory.Connected.View(lifetime, (connectionLifetime, server) =>
+                serverFactory.connected.View(lifetime, (connectionLifetime, protocol) =>
                 {
-                    var serializers = new Serializers();
-
-                    var protocol = new Protocol(Protocol, serializers, new Identities(IdKind.Server),
-                        scheduler, server, connectionLifetime);
-
                     scheduler.Queue(() =>
                     {
                         var model = new DteProtocolModel(connectionLifetime, protocol);
@@ -49,6 +45,57 @@ namespace JetBrains.EnvDTE.Host
             {
                 provider.RegisterCallbacks(model);
             }
+        }
+    }
+
+    // TODO: Move to a common place and make sure that reusing the protocol this way is safe
+    public class ProtocolFactory
+    {
+        public readonly int localPort;
+        public readonly IViewableSet<Protocol> connected = new ViewableSet<Protocol>();
+
+        public ProtocolFactory(Lifetime lifetime, IScheduler scheduler, string id, IPEndPoint? endpoint = null)
+            : this(lifetime, () => new SocketWire.WireParameters(scheduler, id), endpoint) {}
+
+        public ProtocolFactory(
+            Lifetime lifetime,
+            Func<SocketWire.WireParameters> wireParametersFactory,
+            IPEndPoint? endpoint = null
+        )
+        {
+            var serverSocket = SocketWire.Server.CreateServerSocket(endpoint);
+            var serverSocketLifetimeDef = new LifetimeDefinition(lifetime);
+            serverSocketLifetimeDef.Lifetime.OnTermination(() =>
+            {
+                SocketWire.Base.CloseSocket(serverSocket);
+            });
+            localPort = ((IPEndPoint) serverSocket.LocalEndPoint).Port;
+
+            void Rec()
+            {
+                lifetime.TryExecute(() =>
+                {
+                    var (scheduler, id) = wireParametersFactory();
+                    var s = new SocketWire.Server(lifetime, scheduler, serverSocket, id);
+
+                    // Create one protocol per server, this way server instances can be reused for multiple client connections
+                    var proto = new Protocol($"{s.Id}-Protocol", new Serializers(), new Identities(IdKind.Server), scheduler, s, lifetime);
+
+                    // Each server will spawn a thread that will be waiting in serverSocket.Accept method. When lifetime
+                    // termination is invoked, these threads synchronously join the termination thread. Since these Thread.Join
+                    // calls are located deeper in the Lifetime termination stack we have to place this socket termination call
+                    // after each server creation.
+                    lifetime.OnTermination(() => serverSocketLifetimeDef.Terminate());
+
+                    s.Connected.WhenTrue(lifetime, lt =>
+                    {
+                        connected.AddLifetimed(lt, proto);
+                        Rec();
+                    });
+                });
+            }
+
+            Rec();
         }
     }
 }
