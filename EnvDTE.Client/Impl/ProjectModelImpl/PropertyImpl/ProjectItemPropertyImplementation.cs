d7e45e0a0bb9c03be6db6e8cd464629cb23111b6
Veljko Dragic <veljko.dragic@jetbrains.com>
2026-03-02 21:51:14 +0100

    Add barebones `Properties` implementation for `ProjectItem` interface
    Covers the needs of EF6 tools

---
+++ EnvDTE.Client/Impl/ProjectModelImpl/PropertyImpl/ProjectItemPropertyImplementation.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using JetBrains.EnvDTE.Client.Impl.ProjectModelImpl.PropertyImpl.PropertyInfo;
+using JetBrains.Rider.Model;
+
+namespace JetBrains.EnvDTE.Client.Impl.ProjectModelImpl.PropertyImpl;
+
+public class ProjectItemPropertyImplementation(
+    [NotNull] DteImplementation dte,
+    [NotNull] PropertiesImplementation parent,
+    [NotNull] ProjectItemModel projectItemModel,
+    [NotNull] StringPropertyInfo propertyInfo)
+    : PropertyImplementation(dte, parent, propertyInfo)
+{
+    protected override string GetRawValue(string name) =>
+        DteImplementation.DteProtocolModel.ProjectItem_get_Property.Sync(new (name, projectItemModel));
+
+    protected override void SetRawValue(string name, string value) =>
+        DteImplementation.DteProtocolModel.ProjectItem_set_Property.Sync(new (name, value, projectItemModel));
+}
---
+++ EnvDTE.Client/Impl/ProjectModelImpl/PropertyImpl/PropertyInfo/VisualStudioProperties.ProjectItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace JetBrains.EnvDTE.Client.Impl.ProjectModelImpl.PropertyImpl.PropertyInfo;
+
+internal static partial class VisualStudioProperties
+{
+    // TODO: Implement this fully
+    internal static readonly IReadOnlyDictionary<string, StringPropertyInfo> ProjectItemPropertiesMap =
+        new Dictionary<string, StringPropertyInfo>
+        {
+            ["FullPath"] = new("FullPath", "FullPath", true)
+        };
+}
